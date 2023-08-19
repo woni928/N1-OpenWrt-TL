5eb2049d8b0f97baa5fb4d24c1d3f13c1ecf35a7
woni928 <398401414@qq.com>
2023-08-19 21:26:38 +0800

    Update diy.sh

--- config/diy.sh
+++ config/diy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Default IP
-sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.123.2/g' package/base-files/files/bin/config_generate
 
 # Firewall
 echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/config/firewall/files/firewall.user
@@ -16,3 +16,4 @@ rm -rf feeds/packages/net/v2ray-geodata
 svn co https://github.com/nantayo/passwall/trunk package/passwall
 svn co https://github.com/ophub/luci-app-amlogic/trunk package/amlogic
 svn co https://github.com/sbwml/luci-app-mosdns/trunk package/mosdns
+svn co https://github.com/haiibo/openwrt-packages/trunk package/openwrt-packages
